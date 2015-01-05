e1dec2ad84d1a34be433dfb580a0ec5bbecfac20
Revalucien <thundertrispear@gmail.com>
2015-01-05 09:04:50 -0500

    removed main

--- ModelSim/Plot2D.java
+++ ModelSim/Plot2D.java
@@ -8,14 +8,10 @@ public class Plot2D{
     private Frame fr;  
 
     public Plot2D(){
-	
-	setSize(600,600);
-	setLocation(100,100);
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-
-    public static void main(String[] args){
-	Plot2D p = new Plot2D();
-	p.setVisible(true);
+	fr = new Frame("Graph");
+	fr.setSize(600,600);
+	fr.setLocation(100,100);
+	fr.setUndecorated(false);
+	fr.setVisible(true);
     }
 }
