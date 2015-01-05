a46af9002756884f545a9ddd50a0d4f0d2b4b111
Revalucien <thundertrispear@gmail.com>
2015-01-05 09:07:11 -0500

    testing multiple instances of Plot2D

--- ModelSim/Main.java
+++ ModelSim/Main.java
@@ -5,6 +5,7 @@
 public class Main {
 	public static void main(String[] args) {
 		MWindow window = new MWindow();
-		Plot2D plot = new Plot2D();	
+		Plot2D plot = new Plot2D();
+		Plot2D dup = new Plot2D();
 	}
 }
