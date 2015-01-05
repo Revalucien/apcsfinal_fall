bfeeb169b77fa4950037e3fc692523e7ecc29fb8
Revalucien <thundertrispear@gmail.com>
2015-01-05 09:25:47 -0500

    changed imports, more private variables

--- ModelSim/Plot2D.java
+++ ModelSim/Plot2D.java
@@ -1,18 +1,21 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
 import java.io.*;
 import java.util.*;
-import javax.swing.*;
 
 public class Plot2D{
-    private Frame fr;  
+    private Frame fr;
+    private Graphics gr;
+    private Graphics2D gr2d;
 
     public Plot2D(){
 	fr = new Frame("Graph");
+	gr = fr.getGraphics();
+	gr2d = (Graphics2D) gr;
 	fr.setSize(600,600);
 	fr.setLocation(740,100);
 	fr.setUndecorated(false);
 	fr.setVisible(true);
-	
     }
 }
