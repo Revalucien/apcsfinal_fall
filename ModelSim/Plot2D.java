71f3977cfe04ceae816eca3938e695b8fbbbc689
Revalucien <thundertrispear@gmail.com>
2015-01-06 09:26:32 -0500

    trying to drawline

--- ModelSim/Plot2D.java
+++ ModelSim/Plot2D.java
@@ -8,14 +8,19 @@ public class Plot2D{
     private Frame fr;
     private Graphics gr;
     private Graphics2D gr2d;
+    private Image img;
 
     public Plot2D(){
 	fr = new Frame("Graph");
-	gr = fr.getGraphics();
-	gr2d = (Graphics2D) gr;
 	fr.setSize(600,600);
 	fr.setLocation(740,100);
 	fr.setUndecorated(false);
 	fr.setVisible(true);
+	gr = fr.getGraphics();
+	gr2d = (Graphics2D) gr;
+	gr.setColor(Color.BLACK);
+	gr.drawLine(20,20,30,30);
+	
+
     }
 }
