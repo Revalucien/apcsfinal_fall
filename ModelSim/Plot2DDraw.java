5e73a57a5b9956b39b1eb469b48207b3493be81d
Revalucien <thundertrispear@gmail.com>
2015-01-09 09:24:09 -0500

    slight modification to Plot2DDraw

--- ModelSim/Plot2DDraw.java
+++ ModelSim/Plot2DDraw.java
@@ -1,10 +1,16 @@
 import java.awt.*;
 
-public class Plot2DDraw implements Panel {
-    private panel pn;
+public class Plot2DDraw extends Panel {
+    private Panel pn;
+    private Graphics g;
+
+    public Plot2DDraw(){
+	pn.paint(g);
+    }
 
     @Override
-    public Plot2DDraw () {
-	// pn.paint(Graphics g);
+    public void paint(Graphics g){
+	g.setColor(Color.BLACK);
+	g.drawLine(10,10,20,20);
     }
 }
